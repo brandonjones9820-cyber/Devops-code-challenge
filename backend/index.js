256e5eb943289318292857a20df599b184246ca1
Brandon Jones <ngbeezo@Mac.lan>
2026-04-03 12:40:20 -0500

    Fix backend route to /api

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', '*')
     next();
 })
-app.get(/.*/, (req, res) => {
+app.get('/api', (req, res) => {
     console.log(`${new Date().toISOString()} GET`)
     res.json({ message: `SUCCESS ${ID}` })
 })
